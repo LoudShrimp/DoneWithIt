52f8d48b31ee7e353865688b789450fe77953eef
LoudShrimp <PestLines@gmail.com>
2024-02-05 19:38:19 -0800

    Initital Commit: React native practice

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+
+export default function App() {
+  const handlePress = () => console.log("text pressed")
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>Hello World!</Text>
+      <Image source={{
+        width: 200,
+        height: 300,
+        uri: "https://picsum.photos/200/300"}} />
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
