eed5d6be646c895941980d532826ff9a5562d83d
LoudShrimp <PestLines@gmail.com>
2024-02-05 19:48:46 -0800

    Touchables

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log("text pressed")
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text onPress={handlePress}>Hello World!</Text>
-      <Image source={{
-        width: 200,
-        height: 300,
-        uri: "https://picsum.photos/200/300"}} />
+      <Text onPress={handlePress}>Hello world</Text>
+      <TouchableHighlight onPress={() => console.log("image tapped")}>
+        <Image source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300"}} 
+        />
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
