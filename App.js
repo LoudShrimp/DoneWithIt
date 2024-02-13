8316bbd2c65d678687ef660cae509aaa36baec5c
LoudShrimp <PestLines@gmail.com>
2024-02-13 15:10:43 -0800

    UI updates

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { 
+  Text,
   View
 } from 'react-native';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
+
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
 
   return (
-    <WelcomeScreen />
+    <MessagesScreen />
   );
 }
