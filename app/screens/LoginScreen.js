7a2a9b81d9afbaf20d23cf39b934fd67e56041f9
LoudShrimp <PestLines@gmail.com>
2024-03-04 13:20:29 -0800

    Implementing form validation with Yup

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Image, StyleSheet } from 'react-native';
 import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 import Screen from '../components/Screen';
+import AppText from '../components/AppText';
 import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
+
+
+const validationSchema = Yup.object().shape({
+    email: Yup.string().required().email().label("Email"),
+    password: Yup.string().required().min(4).label("Password")
+});
 
 function LoginScreen(props) {
 
@@ -18,8 +26,9 @@ function LoginScreen(props) {
             <Formik
                 initialValues={{ email: '', password: '' }}
                 onSubmit={values => console.log(values)}
+                validationSchema={validationSchema}
             >
-                {({ handleChange, handleSubmit }) => (
+                {({ handleChange, handleSubmit, errors }) => (
                     <>
                         <AppTextInput 
                             autoCapitalize="none"
@@ -30,6 +39,7 @@ function LoginScreen(props) {
                             placeholder="Email"
                             textContentType="emailAddress"
                         />
+                        <AppText style={{ color: 'red' }}>{errors.email}</AppText>
                         <AppTextInput 
                             autoCapitalize="none"
                             autoCorrect={false}
@@ -39,6 +49,7 @@ function LoginScreen(props) {
                             secureTextEntry 
                             textContentType="password"
                         />
+                        <AppText style={{ color: 'red' }}>{errors.password}</AppText>
                         <AppButton title="Login" onPress={handleSubmit} />
                     </>
                 ) }
