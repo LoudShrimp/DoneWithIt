6163a65d89e31c82de5968e96840f57176fac4a3
LoudShrimp <PestLines@gmail.com>
2024-03-11 12:16:12 -0700

    Improving the listing edit screen

--- App.js
+++ App.js
@@ -4,35 +4,9 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import ImageInput from "./app/components/ImageInput";
 import { Button, Image } from "react-native";
+import ImageInputList from "./app/components/ImageInputList";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  const [ imageUri, setImageUri] = useState();
-
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (!granted)
-      alert('You need to enable permission to access the library.');
-  }
-  
-  useEffect(() => {
-    requestPermission();
-  }, [])
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.canceled)
-        setImageUri(result.assets[0].uri)
-    } catch (error) {
-      console.log('Error reading an image', error);
-    }
-  }
-
-  return (
-    <Screen>
-      <ImageInput 
-        imageUri={imageUri}
-        onChangeImage={uri => setImageUri(uri)}/>
-    </Screen>
-  );
+  return <ListingEditScreen />
 }
