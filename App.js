24e8455d2a6c5d3f5c1149a8d79c71a92f8e7e1c
LoudShrimp <PestLines@gmail.com>
2024-03-11 11:15:53 -0700

    Building the image input layout

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import React, { useState } from "react";
-import LoginScreen from "./app/screens/LoginScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import ListItem from "./app/components/ListItem";
-import ListingScreen from "./app/screens/ListingScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+import React, { useEffect, useState } from "react";
+import Screen from "./app/components/Screen";
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+import ImageInput from "./app/components/ImageInput";
+import { Button, Image } from "react-native";
 
 export default function App() {
+  const [ imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted)
+      alert('You need to enable permission to access the library.');
+  }
+  
+  useEffect(() => {
+    requestPermission();
+  }, [])
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled)
+        setImageUri(result.assets[0].uri)
+    } catch (error) {
+      console.log('Error reading an image', error);
+    }
+  }
+
   return (
-    <ListingEditScreen />
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+      <ImageInput imageUri={imageUri}/>
+    </Screen>
   );
 }
