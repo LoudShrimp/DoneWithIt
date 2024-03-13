b85e081ce4b15654695d583daf00c99e731b8d25
LoudShrimp <PestLines@gmail.com>
2024-03-12 20:36:34 -0700

    Added the register screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 import AppButton from '../components/Button';
 
-function WelcomeScreen(props) {
+function WelcomeScreen({ navigation }) {
     return (
         <ImageBackground 
             blurRadius={10}
