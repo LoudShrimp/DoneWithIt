2aa8f54aeeef7ada809969ac0d5817a1332a507a
LoudShrimp <PestLines@gmail.com>
2024-03-14 14:03:39 -0700

    Refactoring tabs

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
-import React, { useEffect, useState } from "react";
-import Screen from "./app/components/Screen";
-import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
-import ImageInput from "./app/components/ImageInput";
-import { Button, Image } from "react-native";
-import ImageInputList from "./app/components/ImageInputList";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, {  } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+
 
 export default function App() {
   return (
--- app/navigation/FeedNavigator.js
+++ app/navigation/FeedNavigator.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import ListingScreen from '../screens/ListingScreen';
-import ListingDetailsScreen from '../screens/ListingDetailsScreen';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import ListingScreen from "../screens/ListingScreen";
+import ListingDetailsScreen from "../screens/ListingDetailsScreen";
 
 const Stack = createStackNavigator();
 
 const feedNavigator = () => (
-    <Stack.Navigator mode="modal">
-        <Stack.Screen name="Listings" component={ListingScreen}/>
-        <Stack.Screen 
-            name="ListingDetails" 
-            component={ListingDetailsScreen} 
-            options={{headerShown: false}}
-        />
-    </Stack.Navigator>
-)
+  <Stack.Navigator mode="modal" screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Listings" component={ListingScreen} />
+    <Stack.Screen name="ListingDetails" component={ListingDetailsScreen} />
+  </Stack.Navigator>
+);
 
 export default feedNavigator;
