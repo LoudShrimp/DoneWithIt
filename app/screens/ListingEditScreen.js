7f3de534543ac76cfa8feb360c350404dd203d1b
LoudShrimp <PestLines@gmail.com>
2024-04-02 20:42:00 -0700

    resetting the form

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -85,7 +85,7 @@ function ListingEditScreen() {
   // const [uploadVisible, setUploadVisible] = useState(false);
   const [progress, setProgress] = useState(0);
 
-  const handleSubmit = async (listing) => {
+  const handleSubmit = async (listing, { resetForm }) => {
     setProgress(0);
     // setUploadVisible(true);
     const result = await listingsApi.addListing(
@@ -100,6 +100,7 @@ function ListingEditScreen() {
     }
     alert("New listing succesfully created");
     // console.log("Success");
+    resetForm();
   };
 
   return (
