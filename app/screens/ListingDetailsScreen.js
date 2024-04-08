473437501aada9531a200da5dc6ea799febbe307
LoudShrimp <PestLines@gmail.com>
2024-04-08 14:40:59 -0700

    caching images

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,52 +1,58 @@
-import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Image } from "react-native-expo-image-cache";
 
-import ListItem from '../components/ListItem';
-import AppText from '../components/Text';
-import colors from '../config/colors';
+import ListItem from "../components/ListItem";
+import AppText from "../components/Text";
+import colors from "../config/colors";
 
 function ListingDetailsScreen({ route }) {
-    const listing = route.params;
+  const listing = route.params;
 
-    return (
-        <View>
-            <Image style={styles.image} source={listing.image} />
-            <View style={styles.detailsContainer}>
-                <AppText style={styles.title}>{listing.title}</AppText>
-                <AppText style={styles.price}>${listing.price}</AppText>
-                <View style={styles.userContainer}>
-                    <ListItem
-                        image={require("../assets/matt.jpg")}
-                        title="Matthew Phapakdy"
-                        subTitle="5 Listings"
-                    />
-                </View>
-            </View>
+  return (
+    <View>
+      <Image
+        style={styles.image}
+        preview={{ uri: listing.images[0].thumbnailUrl }}
+        tint="light"
+        uri={listing.images[0].url}
+      />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>{listing.title}</AppText>
+        <AppText style={styles.price}>${listing.price}</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/matt.jpg")}
+            title="Matthew Phapakdy"
+            subTitle="5 Listings"
+          />
         </View>
-    );
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    detailsContainer: {
-        padding: 20
-    },
-    image: {
-        width: '100%',
-        height: 300
-    },
-    price: {
-        color: colors.secondary,
-        fontWeight: 'bold',
-        fontSize: 20,
-        marginVertical: 10
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: "500",
-    },
-    userContainer: {
-        marginVertical: 40
-    }
-})
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  userContainer: {
+    marginVertical: 40,
+  },
+});
 
 export default ListingDetailsScreen;
