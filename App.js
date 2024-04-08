de82b3c425f36bf2899d1c070795b342b202af71
LoudShrimp <PestLines@gmail.com>
2024-04-08 14:01:52 -0700

    caching succesful request

--- App.js
+++ App.js
@@ -7,17 +7,9 @@ import { View } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-  const demo = async () => {
-    try {
-      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
-      const value = AsyncStorage.getItem("person");
-      const person = JSON.parse(value);
-      console.log(person);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  demo();
-  return null;
+  return (
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
