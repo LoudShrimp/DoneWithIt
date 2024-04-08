cffe0b71850dc7b4e76b0daff4a03844e1f82646
LoudShrimp <PestLines@gmail.com>
2024-04-08 13:53:31 -0700

    implementing a cache layer

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import React, {  } from "react";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
-
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
+import { View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-  return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
-  );
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
+      const value = AsyncStorage.getItem("person");
+      const person = JSON.parse(value);
+      console.log(person);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  demo();
+  return null;
 }
